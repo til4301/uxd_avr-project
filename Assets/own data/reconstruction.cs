3f1c52778585c1e8362960912e20c8334d9d24a3
til4301 <til4301@thi.de>
2020-11-25 16:33:41 +0100

    scenery, popups and reconstruction works

---
+++ Assets/own data/reconstruction.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reconstruction : MonoBehaviour
+{
+    public GameObject templeObject;
+    bool reconstruct = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (Input.GetKeyDown(KeyCode.R)){
+            if (reconstruct){
+              templeObject.transform.localScale = new Vector3(0, 0, 0);
+              Debug.Log ("Reconstruction is enabled and now turned off (2)");
+              reconstruct = !reconstruct;
+            }
+            else if (!reconstruct){
+              templeObject.transform.localScale = new Vector3(1, 1, 1);
+              Debug.Log ("Reconstruction is disbaled and now turned on (1)");
+              reconstruct = !reconstruct;
+            }
+            
+        } 
+    }
+}
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class triggerCollider : MonoBehaviour
+{
+
+public GameObject uiObject;
+public GameObject infoText;
+
+ void OnTriggerEnter (Collider other){
+   uiObject.SetActive(true);
+ }
+
+ void OnTriggerStay (Collider other){
+   if (Input.GetKeyDown(KeyCode.I)){
+      infoText.SetActive(true);
+   }
+   else if (Input.GetKeyDown(KeyCode.Escape)){
+      infoText.SetActive(false);
+   }
+
+           
+           //infoText1.SetActive(!infoText1.activeSelf);
+          /*  Debug.Log ("i gedrückt");
+            uiObject.GetComponent<Text>().text="Hallo"; 
+        
+ }
+
+ void OnTriggerExit (Collider other){
+   uiObject.SetActive(false);
+   infoText.SetActive(false);
+
+ }
+}
+*/
---
+++ Assets/own data/triggerCollider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class triggerCollider : MonoBehaviour
+{
+
+public GameObject uiObject;
+public GameObject infoText;
+
+ void OnTriggerEnter (Collider other){
+   uiObject.SetActive(true);
+ }
+
+ void OnTriggerStay (Collider other){
+   if (Input.GetKeyDown(KeyCode.I)){
+      infoText.SetActive(true);
+   }
+   else if (Input.GetKeyDown(KeyCode.Escape)){
+      infoText.SetActive(false);
+   }
+
+           
+           //infoText1.SetActive(!infoText1.activeSelf);
+          /*  Debug.Log ("i gedrückt");
+            uiObject.GetComponent<Text>().text="Hallo"; */
+        
+ }
+
+ void OnTriggerExit (Collider other){
+   uiObject.SetActive(false);
+   infoText.SetActive(false);
+
+ }
+}
